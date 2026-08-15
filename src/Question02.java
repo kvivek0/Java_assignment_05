53f1a3c9e1c4a4fac620eb5464eab29aaf93fa3d
kvivek0 <vivekksp963@gmail.com]>
2026-08-15 16:03:09 +0530

    Added Q.2

---
+++ src/Question02.java
@@ -0,0 +1,11 @@
+// Q.2 Write a program to print the first 10 natural numbers;
+
+public class Question02 {
+    public static void main(String []args){
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println("N="+i);
+        }
+
+    }
+}
