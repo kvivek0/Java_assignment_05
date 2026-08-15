fcdcfbdaf7e0c27ddd412610d4b1194f618730b3
kvivek0 <vivekksp963@gmail.com]>
2026-08-15 15:55:13 +0530

    first commit

---
+++ src/Question01.java
@@ -0,0 +1,23 @@
+// Q.1 Write a program to print MySirG 5 times on the screen.
+
+public class Question01 {
+    public static void main(String []args){
+        int i,j=1,k=1;
+        for(i=1;i<=5;i++){
+            System.out.println("MYSIRG");
+        }
+        //using Do-While loop;
+        do{
+            System.out.println("shivam");
+            j++;
+        }while(j<=5);
+        // using while loop;
+        while(k<=5){
+            System.out.println("vivek");
+            k++;
+        }
+
+    }
+
+
+}
