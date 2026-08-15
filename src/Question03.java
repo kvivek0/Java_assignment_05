9575de9b44cef669d0da13f629d193f558834f1e
kvivek0 <vivekksp963@gmail.com]>
2026-08-15 16:08:45 +0530

    added Q.03

---
+++ src/Question03.java
@@ -0,0 +1,11 @@
+//Q.3 Write a program to print the first 10 natural numbers in reverse order.
+
+public class Question03 {
+    public static void main(String []args){
+        int i;
+        for(i=10;i>=1;i--){
+            System.out.println("i="+i);
+        }
+
+    }
+}
